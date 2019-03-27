e83f85d864a119e9682d2d14543fe164514e5850
arinmsn <arinmsn@gmail.com>
2019-03-27 07:46:37 -0700

    Issue: Cannot create tables once connected to JAWS database

--- config/connection.js
+++ config/connection.js
@@ -6,20 +6,20 @@ var connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "password",
-        database: "burgers_db"
-    });
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db'
+  });
 };
 
-connection.connect(function(err) {
-    if (err) {
-        console.error("Error connecting:  " + err.stack);
-        return;
-    }
-    console.log("Connected as id: " + connection.threadId + `\n`);
-});
-
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error("Error connecting:  " + err.stack);
+//         return;
+//     }
+//     console.log("Connected as id: " + connection.threadId + `\n`);
+// });
+connection.connect();
 module.exports = connection;
